cf3e080857ca189d370b400d0674b01abe19f34f
quimpery <lautarogomgoda@gmail.com>
2024-10-11 18:18:32 -0300

    Inicio repositorio

---
+++ Guia3.1/Mesa.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guia3._1
+{
+    internal class Mesa:Producto
+    {
+        private double largo;
+
+
+        public Mesa(string descripcion, double precioBase, int tipoMaterial, double largo) : base(descripcion, precioBase, tipoMaterial)
+        {
+            base.TipoMaterial = tipoMaterial;
+            base.precioBase = precioBase;
+            this.largo = largo;
+        }
+
+        public override double CalcularPrecio(double precioBase,double largo,int tipoMaterial)
+        {
+            double precioMesa = 0;
+
+            if(tipoMaterial == 0)
+            {
+                precioMesa = (precioBase * largo)*(1+tipoMaterial);
+            }
+            else if(tipoMaterial == 1)
+            {
+                precioMesa = (precioBase * largo) * (1 + tipoMaterial);
+            }
+            else
+            {
+                precioMesa=(precioBase * largo)*(1 + tipoMaterial);
+            }
+            return precioMesa;            
+        }
+        public override string VerDetalle()
+        {
+            return
+        }
+    }
+}
---
+++ Guia3.1/Producto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guia3._1
+{
+    internal abstract class Producto
+    {
+        private string descripcion;
+        protected double precioBase;
+        protected int TipoMaterial;
+
+
+        public Producto(string descripcion, double precioBase, int tipoMaterial)
+        {
+            this.descripcion = descripcion;
+            this.precioBase = precioBase;
+            TipoMaterial = tipoMaterial;
+        }
+
+        public abstract double CalcularPrecio();
+
+        public abstract string VerDetalle();
+    }
+}
